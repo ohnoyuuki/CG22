fa9c9a7880b0cbd35094c2d6fc1d5992c17f233f
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-07-08 15:53:46 +0900

    2-2

--- main.cpp
+++ main.cpp
@@ -390,7 +390,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
 	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 
-	
+	// RootSignature作成。複数設定できるので配列。今回は結果1つだけなので長さ1の配列
+	D3D12_ROOT_PARAMETER rootParameters[2] = {};
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; // CBVを使う
+	rootParameters[0].ShaderVisibility =D3D12_SHADER_VISIBILITY_PIXEL;                 // PixelShaderで使う
+	rootParameters[0].Descriptor.ShaderRegister = 0; // レジスタ番号0とバインド
+	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; // CBVを使う
+	rootParameters[1].ShaderVisibility =D3D12_SHADER_VISIBILITY_VERTEX;              // VertexShaderで使う
+	rootParameters[1].Descriptor.ShaderRegister = 0; // レジスタ番号0を使う
+	descriptionRootSignature.pParameters =
+		rootParameters; // ルートレートパラメータ配列へのポインタ
+	descriptionRootSignature.NumParameters =
+		_countof(rootParameters); // 配列の長さ
 
 	//シリアライズしてバイナリにする
 	ID3DBlob* signatureBlob = nullptr;
