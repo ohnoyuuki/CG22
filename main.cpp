d22651c068de911fc32e8a1bea4431179f9ab0e4
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-07-08 11:24:44 +0900

    0-5

--- main.cpp
+++ main.cpp
@@ -1,36 +1,35 @@
-#include<Windows.h>
-#include<cstdint>
-#include<string>
-#include<format>
+#include <Windows.h>
+#include <cstdint>
+#include <string>
+#include <format>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
 
 //ウィンドウプロシージャ
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
-	WPARAM wparam, LPARAM lparam)
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	//メッセージに応じてゲーム固有の処理を行う
 	switch (msg)
 	{
-		//ウィンドウが破壊された
+		//ウィンドウが破棄された
 	case WM_DESTROY:
 		//OSに対して、アプリの終了を伝える
 		PostQuitMessage(0);
 		return 0;
-
 	}
 	//標準のメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
-
-//文字列を格納する
-std::string str0{ "STRING!!!" };
-//整数を文字列にする
-std::string str1{ std::to_string(10) };
 
 void Log(const std::string& message)
 {
 	OutputDebugStringA(message.c_str());
 }
 
+//string->wstring
 std::wstring ConvertString(const std::string& str)
 {
 	if (str.empty())
@@ -38,7 +37,6 @@ std::wstring ConvertString(const std::string& str)
 		return std::wstring();
 	}
 	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
-
 	if (sizeNeeded == 0)
 	{
 		return std::wstring();
@@ -47,14 +45,13 @@ std::wstring ConvertString(const std::string& str)
 	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
 	return result;
 }
-
-std::string ConverString(const std::wstring& str)
+//wstring->string
+std::string ConvertString(const std::wstring& str)
 {
 	if (str.empty())
 	{
 		return std::string();
 	}
-
 	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
 	if (sizeNeeded == 0)
 	{
@@ -63,26 +60,23 @@ std::string ConverString(const std::wstring& str)
 	std::string result(sizeNeeded, 0);
 	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
 	return result;
-
-
-
 }
 
+//DXGIファクトリーの生成
+IDXGIFactory7* dxgiFactory = nullptr;
+//HRESULTはWindows系のエラーコードで、
+//関数が成功したかどうかをSUCCEEDEDマクロで判定出来る
+HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
 
-
-
-//Windowsアプリでのエントリーポイント（main関数）
+//windowsアプリでのエントリーポイント
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-
-
-
 
 	WNDCLASS wc{};
 	//ウィンドウプロシージャ
 	wc.lpfnWndProc = WindowProc;
 
-	//ウィンドウクラス名（なんでも良い）
+	//ウィンドウクラス名
 	wc.lpszClassName = L"CG2WindowClass";
 
 	//インスタンスハンドル
@@ -94,8 +88,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//ウィンドウクラスを登録する
 	RegisterClass(&wc);
 
-
-
 	//クライアント領域のサイズ
 	const int32_t kClientWidth = 1280;
 	const int32_t kClientHeight = 720;
@@ -103,43 +95,93 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//ウィンドウサイズを表す構造体にクライアント領域を入れる
 	RECT wrc = { 0,0,kClientWidth,kClientHeight };
 
-	//クライアント領域を元に実際のサイズにwrcを変更してもらう
+	//クライアント領域をもとに実際のサイズにwrcを変更してもらう
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
 	//ウィンドウの生成
-	HWND hwnd = CreateWindow
-	(
-		wc.lpszClassName,
-		L"CG2",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		wrc.right - wrc.left,
-		wrc.bottom - wrc.top,
-		nullptr,
-		nullptr,
-		wc.hInstance,
-		nullptr
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,      //利用するクラス名
+		L"CG2",                //タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,   //よく見るウィンドウスタイル
+		CW_USEDEFAULT,         //表示X座標
+		CW_USEDEFAULT,         //表示Y座標
+		wrc.right - wrc.left,  //ウィンドウ横幅
+		wrc.bottom - wrc.top,  //ウィンドウ縦幅
+		nullptr,               //親ウィンドウハンドル
+		nullptr,               //メニューハンドル
+		wc.hInstance,          //インスタンスハンドル
+		nullptr                //オプション
 	);
+
 	//ウィンドウを表示する
 	ShowWindow(hwnd, SW_SHOW);
+
 	MSG msg{};
-	//ウィンドウの✖ボタンが押されるまでループ
+
+	//ウィンドウの×ボタンが押されるまでループ
 	while (msg.message != WM_QUIT)
 	{
-		//Widowにメッセージが来てたら最優先で処理させる
+		//Windowにメッセージが来てたら最優先で処理させる
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+			DispatchMessageW(&msg);
 		} else
 		{
 			//ゲームの処理
 		}
 	}
-	//出力ウィンドウへの文字出力
-	OutputDebugStringA("Hello,DirectX!\n");
 
-	Log(ConverString(std::format(L"WSTRING{}\n", L"abc")));
+	//wstring->string
+	Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
+
+
+
+	//初期化の根本的な部分でエラーが出た場合はプログラムが間違っているか、どうにもできない場合が多いのでassertにする
+	assert(SUCCEEDED(hr));
+
+
+	//使用するアダプタ用の変数
+	IDXGIAdapter4* useAdapter = nullptr;
+
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; i++)
+	{
+		//アダプタ-の情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE))
+		{
+			//採用したアダプタの情報をログに出力
+			Log(ConvertString(std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
+			break;
+		}
+		useAdapter = nullptr;//ソフトウェアアダプタの場合は見なかったことにする
+	}
+	//適切なアダプタが見つからなかったので起動できない
+	assert(useAdapter != nullptr);
+
+	ID3D12Device* device = nullptr;
+	//機能レベルをログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0 };
+	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
+	for (size_t i = 0; i < _countof(featureLevels); i++)
+	{
+		//採用したアダプターでデバイスを作成
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+		//指定した機能レベルでデバイスが生成できたかを確認
+		if (SUCCEEDED(hr))
+		{
+			//生成できたのでログ出力を行ってループを抜ける
+			Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+	//デバイスの生成がうまくいかなかったので起動できない
+	assert(device != nullptr);
+	Log("Complate create D3D12Device!!!\n");//初期化完了のログを出す
+
 
 	return 0;
+
 }
