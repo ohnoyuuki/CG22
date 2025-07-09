f51e94a966e73a22e12e0f9e324226b3cb51a528
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-07-09 16:57:10 +0900

    kansei

--- main.cpp
+++ main.cpp
@@ -869,7 +869,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		{
 			//ゲームの処理
 			 
-			 
+			transform.rotate.y += 0.03f;
+
+			Matrix4x4 worldMatrix = MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
+			Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate);
+			Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+			Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kClientWidth) / float(kClientHeight), 0.1f, 100.0f);
+			Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+			*wvpData = worldViewProjectionMatrix;
+
 			
 				//更新処理をかく
 		//これからもよろしくお願いします。
@@ -1024,6 +1032,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	debugController->Release();
 #endif
 
+	wvpResource->Release();
 	materialResource->Release();
 	CloseWindow(hwnd);
 
