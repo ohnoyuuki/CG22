364c7e415459252f8f36e61529042c2262e9ec06
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-07-29 10:02:43 +0900

    m

--- main.cpp
+++ main.cpp
@@ -62,8 +62,16 @@ struct Transform {
 	Vector3 translate;
 };
 
-struct ModelData {
+struct MaterialData
+{
+	std::string textureFilePath;
+};
+
+
+struct ModelData
+{
 	std::vector<VertexData>vertices;
+	MaterialData material;
 
 };
 
@@ -481,6 +489,7 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string&filena
 			s >> normal.x >> normal.y >> normal.z;
 			normals.push_back(normal);
 		}else if(identifier == "f"){
+			VertexData triangle[3];
 			//面は三角形限定。その他は未対応
 			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
 				std::string vertexDefinition;
@@ -495,23 +504,59 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string&filena
 				}
 				//要素へのIndexから、実際の要素の値を所得して、頂点を構築する
 				Vector4 position = positions[elementIndices[0] - 1];
+				position.x *= -1.0f;
 				Vector2 texcoord = texcoords[elementIndices[1] - 1];
 				Vector3 normal = normals[elementIndices[2] - 1];
-				VertexData vertex = { position,texcoord};
+				//normal.x *= -1.0f;
+				//VertexData vertex = { position,texcoord};
 				//VertexData vertex = { position,texcoord,normal };
-				modelData.vertices.push_back(vertex);
-
+				//modelData.vertices.push_back(vertex);
+				triangle[faceVertex] = { position,texcoord};
+				//triangle[faceVertex] = { position,texcoord,normal };
 			}
+			//頂点を逆順で登録することで、回り順を逆にする
+			modelData.vertices.push_back(triangle[2]);
+			modelData.vertices.push_back(triangle[1]);
+			modelData.vertices.push_back(triangle[0]);
 		}
 
 	}
 
 	//４，ModelDataを返す
 	return modelData;
-
-
 }
 
+//MaterialData読み込み関数
+MaterialData LoatMaterialTemplateFile(const std::string& directoryPath,const std::string&filename)
+{
+	//１．中で必要となる変数の宣言
+	MaterialData materialData;//構築するMaterialData
+	std::string line;//ファイルから読んだ１行を格納するもの
+	
+	//２．ファイルを開く
+	std::ifstream file(directoryPath + "/" + filename);//ファイルを開く
+	assert(file.is_open());//とりあえず開けなかったら止める
+
+	//３．実際にファイルを読み、MaterialDataを構築していく
+	while(std::getline(file,line))
+	{
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		//identifierに応じた処理
+		if(identifier == "map_Kd")
+		{
+			std::string textureFilename;
+			s >> textureFilename;
+			//連結してファイルパスにする
+			materialData.textureFilePath = directoryPath + "/" + textureFilename;
+		}
+
+	}
+	//４．MaterialDataを返す
+	return materialData;
+}
 
 
 //ウィンドウプロシージャ
@@ -1076,6 +1121,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		IID_PPV_ARGS(&graphicsPipelineState));
 	assert(SUCCEEDED(hr));
 
+
+
+	//三角形２個
+	/*
 	ID3D12Resource* vertexResource = CreateBufferResource(device, sizeof(VertexData) * 6);
 
 	//頂点バッファビューを作成する
@@ -1113,6 +1162,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	vertexData[5].position = { 0.5f,-0.5f,-0.5f,1.0f };
 	vertexData[5].texcoord = { 1.0f,1.0f };
 
+	*/
+
+	//モデル読み込み
+	ModelData modelData = LoadObjFile("resources", "plane.obj");
+	//頂点リソースを作る
+	ID3D12Resource* vertexResource = CreateBufferResource(device, sizeof(VertexData) * modelData.vertices.size());
+	//頂点バッファビューを作成する
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();//リソースの先頭のアドレスから使う
+	vertexBufferView.SizeInBytes = UINT(sizeof(VertexData) * modelData.vertices.size());//使用するリソースのサイズは頂点のサイズ
+	vertexBufferView.StrideInBytes = sizeof(VertexData);//１頂点あたりのサイズ
+
+	//頂点リソースにデータを書き込む
+	VertexData* vertexData = nullptr;
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));//書き込むためのアドレスを取得
+	std::memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData)* modelData.vertices.size());
 
 	//ビューボート
 	D3D12_VIEWPORT viewport{};
@@ -1131,6 +1196,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	scissorRect.right = kClientWidth;
 	scissorRect.top = 0;
 	scissorRect.bottom = kClientHeight;
+
+
 
 
 
@@ -1295,7 +1362,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 
 
-			transform.rotate.y += 0.03f;
+		
 			Matrix4x4 worldMatrix = MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
 			Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate);
 			Matrix4x4 viewMatrix = Inverse(cameraMatrix);
@@ -1308,8 +1375,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 			ImGui::Begin("Settings");
 			ImGui::ColorEdit4("material", &materialData->x, ImGuiColorEditFlags_AlphaPreview);//RGBWの指定
+			ImGui::DragFloat("rotate.y", &transform.rotate.y, 0.1f);
 			ImGui::End();
 
+
+			//transform.rotate.y += 0.03f;
+			
 			//ImGuiの内部コマンドを生成する
 			ImGui::Render();
 
@@ -1371,8 +1442,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			commandList->IASetIndexBuffer(&indexBufferViewSprite);//IBNを設定
 
 			//描画!(DrawCall/ドローコル）。３頂点で一つのインスタンス。インスタンスについては今後
-			commandList->DrawInstanced(6, 1, 0, 0);
+			//commandList->DrawInstanced(6, 1, 0, 0);
 
+			//モデル描画
+			commandList->DrawInstanced(UINT(modelData.vertices.size()), 1, 0, 0);
 
 			//--------------------------------------
 
