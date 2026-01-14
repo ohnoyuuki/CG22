34abf8b28c12272cb6f20b918e21af649a92469a
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-01-14 12:04:28 +0900

    kansei

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -27,7 +27,11 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     if (gMaterial.enableLighting != 0){
-        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        
+        //half lambert
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     }
     else{
