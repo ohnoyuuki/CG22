e202f13cf8bba8d75c0409159fd44574a188b9fc
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-01-14 10:40:25 +0900

    itiokansei

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -26,6 +26,13 @@ PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-    output.color = gMaterial.color * textureColor;
+    if (gMaterial.enableLighting != 0){
+        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    }
+    else{
+        output.color = gMaterial.color * textureColor;
+        
+    }
     return output;
 }
--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -17,8 +17,9 @@ struct VertexShaderInput
 
 VertexShaderOutput main(VertexShaderInput input)
 {
-    VertexShaderOutput output;
+    VertexShaderOutput output;  
     output.position = mul(input.position, gTransformationMatrix.WVP);
     output.texcoord = input.texcoord;
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
     return output;
 }
